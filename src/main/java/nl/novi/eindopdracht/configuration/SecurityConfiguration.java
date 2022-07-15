8202babc04dec912f822f2930e23f9e342492c07
jentebuijs <jentebuijs@gmail.com>
2022-07-15 16:13:22 +0200

    JWT service, requestfilter, authcontroller, authdto, securityconfiguration toegevoegd

---
+++ src/main/java/nl/novi/eindopdracht/configuration/SecurityConfiguration.java
@@ -0,0 +1,54 @@
+package nl.novi.eindopdracht.configuration;
+
+import nl.novi.eindopdracht.filters.JwtRequestFilter;
+import nl.novi.eindopdracht.services.JwtService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    JwtService jwtService;
+
+    @Bean
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return super.userDetailsService();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder amb) throws Exception {
+        amb
+                .inMemoryAuthentication()
+                .withUser("karel").password("{noop}appel").roles("ARTIST");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .httpBasic()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests().antMatchers(HttpMethod.POST, "/auth").permitAll()
+                .and()
+                .authorizeRequests().anyRequest().authenticated()
+                .and()
+                .addFilterBefore(new JwtRequestFilter(jwtService, userDetailsService()), UsernamePasswordAuthenticationFilter.class)
+                .csrf().disable();
+    }
+}
