537cc56873a2e804dfe6bcb05b47ff7ecb9df7c4
jentebuijs <jentebuijs@gmail.com>
2022-08-01 12:22:21 +0200

    putmapping profilecontroller opgelost

--- src/main/java/nl/novi/eindopdracht/repositories/UserRepository.java
+++ src/main/java/nl/novi/eindopdracht/repositories/UserRepository.java
@@ -3,7 +3,6 @@ package nl.novi.eindopdracht.repositories;
 import nl.novi.eindopdracht.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
